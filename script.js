48bdf3bea9c43726fc157a29421d891f009d0869
Evan Buckmaster <evanbuckmaster@gmail.com>
2022-04-28 13:44:27 -0400

    created 4x4 grid layout

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let divContainer = document.querySelector('.container');
-console.log(divContainer);
+let etchContainer = document.getElementById('etch_container');
+console.log(etchContainer);
 
 for (let i = 1; i <= 16; i++) {
     let newDiv = document.createElement('div');
-    newDiv.classList.add([i]);
-    newDiv.setAttribute('style', 'border: 1px solid blue; width: 250px; height: 250px;');
-    divContainer.appendChild(newDiv);
+    newDiv.classList.add('box');
+    newDiv.setAttribute('style', 'border: 1px solid blue; width: 100%; height:100%');
+    etchContainer.appendChild(newDiv);
 }
