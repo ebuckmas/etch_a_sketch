c870758e84ba017237c5ee18ae5c49e72526f802
Evan B <evanbuckmaster@gmail.com>
2022-04-28 15:53:40 -0400

    cant get reset button to work

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-let etchContainer = document.getElementById('etch_container');
-console.log(etchContainer);
+const etchContainer = document.getElementById('etch_container');
+const resetButton = document.getElementById('reset');
 
-for (let i = 1; i <= 16; i++) {
+
+for (let i = 1; i <= 256; i++) {
     let newDiv = document.createElement('div');
     newDiv.classList.add('box');
-    newDiv.setAttribute('style', 'border: 1px solid blue; width: 100%; height:100%');
+    newDiv.setAttribute('style', 'border: 1px solid blue; width: 100%; height:100%;');
+    newDiv.addEventListener('mouseover', () => newDiv.setAttribute('style', 'background-color: lightblue;'));
     etchContainer.appendChild(newDiv);
 }
+
+const divs = document.getElementsByClassName('box');
+console.log(divs);
+
+function resetDivs() {
+    divs.setAttribute('style','background-color: white;');
+}
+
+resetButton.addEventListener('click', resetDivs);
